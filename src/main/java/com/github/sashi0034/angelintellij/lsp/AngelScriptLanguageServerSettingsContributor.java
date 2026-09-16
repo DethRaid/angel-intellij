f70ec2cf69afd6c9997ea7f1f8e52f02b2509c65
Sarah Dubois <sarahduboisengineer@gmail.com>
2026-09-15 20:30:38 -0700

    adding settings definitions

--- src/main/java/com/github/sashi0034/angelintellij/lsp/AngelScriptLanguageServerSettingsContributor.java
+++ src/main/java/com/github/sashi0034/angelintellij/lsp/AngelScriptLanguageServerSettingsContributor.java
@@ -23,6 +23,7 @@ public class AngelScriptLanguageServerSettingsContributor extends LanguageServer
 
         @Override
         public String getDefaultConfigurationSchemaContent() {
+            // sheme generate by hand from `server/src/core/settings.js` in the language server
             // language=json
             return """                  
                   {
@@ -33,36 +34,35 @@ public class AngelScriptLanguageServerSettingsContributor extends LanguageServer
                     "type": "object",
                     "additionalProperties": false,
                     "properties": {
-                      "angelscript.language-server.ls-path": {
+                        "angelscript.suppressAnalyzerErrors": {
+                            "type": "boolean",
+                            "title": "AngelScript: Suppress analyzer errors"
+                        },
+                        "angelscript.includePath": {
+                            "type": "string[]",
+                            "title": "AngelScript: additional include paths",
+                            "description": "places to search for files to include"
+                        },
+                        "angelscript.forceIncludePredefined": {
+                            "type": "string[]",
+                            "title": "AngelScript: files to force-include",
+                            "description": "list of files to be included before compiling any other file"
+                        },
+                        "angelscript.implicitMutualInclusion": {
+                            "type": "boolean",
+                            "default": "false",
+                            "description": "Tells the language server to combine all your files into one big file before compiling"
+                        },
+                      "angelscript.default-string-type": {
                         "type": "string",
-                        "title": "Language Server: Path",
-                        "description": "Path to the language server executable. You won\\u0027t need this in most cases, set this only when needing a specific version of the language server"
+                        "title": "AngelScript: Default string type",
+                        "description": "Which type to use for strings. Should probably be `string` or `String`"
                       },
-                      "angelscript.language-server.runtime": {
+                      "angelscript.builtin-array-type": {
                         "type": "string",
-                        "title": "Language Server: Runtime",
-                        "description": "Path to the node executable used to execute the language server. You won\\u0027t need this in most cases"
+                        "title": "AngelScript: Default array type",
+                        "description": "Which type to use for builtin arrays"
                       },
-                      "angelscript.trace.server": {
-                        "type": "string",
-                        "enum": [
-                          "off",
-                          "messages",
-                          "verbose"
-                        ],
-                        "default": "off",
-                        "description": "Traces the communication between VS Code and the language server."
-                      },
-                      "angelscript.content-intellisense": {
-                        "type": "boolean",
-                        "default": false,
-                        "description": "Enable experimental support for content collection intellisense inside Markdown, MDX and Markdoc. Note that this require also enabling the feature in your angelscript config (experimental.contentCollectionIntellisense) (angelscript 4.14+)"
-                      },
-                      "angelscript.updateImportsOnFileMove.enabled": {
-                        "type": "boolean",
-                        "default": false,
-                        "description": "Controls whether the extension updates imports when a file is moved to a new location. In most cases, you\\u0027ll want to keep this disabled as TypeScript and the angelscript TypeScript plugin already handles this for you. Having multiple tools updating imports at the same time can lead to corrupted files."
-                      }
                     }
                   }""";
         }
